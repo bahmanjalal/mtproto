89a7db6315661a05a8ee9a0427e4974a86aef5e5
bahmanjalal <58664994+bahmanjalal@users.noreply.github.com>
2021-08-31 13:45:18 +0430

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -102,7 +102,7 @@ install(){
     cp -f $WORKDIR/MTProxy/objs/bin/mtproto-proxy $WORKDIR
     cd $WORKDIR
   else
-    wget https://raw.githubusercontent.com/bahmanjalal/mtproto/master/mtproto-proxy -O mtproto-proxy -q
+    https://github.com/ellermister/mtproxy/releases/download/0.02/mtproto-proxy -O mtproto-proxy -q
     chmod +x mtproto-proxy
   fi
 }
